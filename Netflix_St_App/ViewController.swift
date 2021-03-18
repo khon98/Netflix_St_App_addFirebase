6dba6d658547016f1a391d8e08a80e7990d90617
rlaghs <rlaghs@naver.com>
2021-03-18 19:49:16 +0900

    Initial Commit

---
+++ Netflix_St_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_St_App
+//
+//  Created by 김혼 on 2021/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
